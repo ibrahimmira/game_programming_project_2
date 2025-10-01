74637f35bf3f348f4f357383b87ab4c41dba85e3
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2025-10-01 10:38:24 -0400

    Added main file

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/**
+* Author: [Your name here]
+* Assignment: Pong Clone
+* Date due: 2025-10-13, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
