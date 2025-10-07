0aae16bdb8397a4bfe3e2b41889c28bacffa94d9
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2025-10-07 15:42:33 -0400

    Initial setup

--- main.cpp
+++ main.cpp
@@ -7,3 +7,181 @@
 * NYU School of Engineering Policies and Procedures on
 * Academic Misconduct.
 **/
+
+#include "CS3113/cs3113.h"
+
+// Global Constants
+constexpr int SCREEN_WIDTH  = 1600 / 1.1,
+              SCREEN_HEIGHT = 900 / 1.1,
+              FPS           = 60,
+              SIZE          = 500 / 2,
+              SPEED         = 200;
+
+constexpr char    BG_COLOUR[]    = "#424242ff";
+constexpr Vector2 ORIGIN         = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 },
+                  BASE_SIZE      = { (float) SIZE, (float) SIZE },
+                  INIT_POS  = { ORIGIN.x, ORIGIN.y };
+                  
+constexpr char SLIDER1[] = "assets/game/slider.png";
+
+// Global Variables
+AppStatus gAppStatus     = RUNNING;
+float     gAngle         = 0.0f,
+          gPreviousTicks = 0.0f;
+
+Vector2 gPosition  = INIT_POS,
+        gMovement  = { 0.0f, 0.0f },
+        gScale     = BASE_SIZE,
+        gMousePosition = GetMousePosition();
+
+Texture2D gSLIDER1;
+
+unsigned int startTime;
+
+// Function Declarations
+void initialise();
+void processInput();
+void update();
+void render();
+void shutdown();
+bool isColliding(const Vector2 *positionA, const Vector2 *scaleA, const Vector2 *positionB, const Vector2 *scaleB);
+void renderObject(const Texture2D *texture, const Vector2 *position, 
+                  const Vector2 *scale);
+
+void initialise()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "User Input / Collision Detection");
+
+    startTime = time(NULL);
+
+    gSLIDER1  = LoadTexture(SLIDER1);
+
+    SetTargetFPS(FPS);
+}
+
+void processInput() 
+{
+    gMovement = { 0.0f, 0.0f };
+
+    if      (IsKeyDown(KEY_W)) gMovement.y = -1;
+    else if (IsKeyDown(KEY_S)) gMovement.y =  1;
+
+    /*
+    This system will cause quite a bit of "shaking" once the game object
+    reaches the mouse position. Ideally, we'd stop checking for this
+    once the object reaches a general area AROUND the mouse position.
+    */
+   
+    // to avoid faster diagonal speed
+    if (GetLength(&gMovement) > 1.0f) Normalise(&gMovement);
+
+    if (IsKeyPressed(KEY_Q) || WindowShouldClose()) gAppStatus = TERMINATED;
+}
+
+void update() 
+{
+    // Delta time
+    float ticks = (float) GetTime();
+    float deltaTime = ticks - gPreviousTicks;
+    gPreviousTicks  = ticks;
+    
+    gPosition = {
+    gPosition.x + SPEED * gMovement.x * deltaTime,
+    gPosition.y + SPEED * gMovement.y * deltaTime
+    };
+}
+
+void render()
+{
+    BeginDrawing();
+    ClearBackground(ColorFromHex(BG_COLOUR));
+
+    // Render SLIDER1
+    renderObject(&gSLIDER1, &gPosition, &gScale);
+
+    EndDrawing();
+}
+
+void shutdown() 
+{ 
+    CloseWindow(); 
+    UnloadTexture(gSLIDER1);
+
+}
+
+int main(void)
+{
+    initialise();
+
+    while (gAppStatus == RUNNING)
+    {
+        processInput();
+        update();
+        render();
+    }
+
+    shutdown();
+
+    return 0;
+}
+
+
+// Function Definitions
+
+/**
+ * @brief Checks for a square collision between 2 Rectangle objects.
+ * 
+ * @see 
+ * 
+ * @param positionA The position of the first object
+ * @param scaleA The scale of the first object
+ * @param positionB The position of the second object
+ * @param scaleB The scale of the second object
+ * @return true if a collision is detected,
+ * @return false if a collision is not detected
+ */
+bool isColliding(const Vector2 *positionA,  const Vector2 *scaleA, 
+                 const Vector2 *positionB, const Vector2 *scaleB)
+{
+    float xDistance = fabs(positionA->x - positionB->x) - ((scaleA->x + scaleB->x) / 2.0f);
+    float yDistance = fabs(positionA->y - positionB->y) - ((scaleA->y + scaleB->y) / 2.0f);
+
+    if (xDistance < 0.0f && yDistance < 0.0f) return true;
+
+    return false;
+}
+
+void renderObject(const Texture2D *texture, const Vector2 *position, 
+                  const Vector2 *scale)
+{
+    // Whole texture (UV coordinates)
+    Rectangle textureArea = {
+        // top-left corner
+        0.0f, 0.0f,
+
+        // bottom-right corner (of texture)
+        static_cast<float>(texture->width),
+        static_cast<float>(texture->height)
+    };
+
+    // Destination rectangle – centred on gPosition
+    Rectangle destinationArea = {
+        position->x,
+        position->y,
+        static_cast<float>(scale->x),
+        static_cast<float>(scale->y)
+    };
+
+    // Origin inside the source texture (centre of the texture)
+    Vector2 originOffset = {
+        static_cast<float>(scale->x) / 2.0f,
+        static_cast<float>(scale->y) / 2.0f
+    };
+
+    // Render the texture on screen
+    DrawTexturePro(
+        *texture, 
+        textureArea, destinationArea, originOffset,
+        gAngle, WHITE
+    );
+}
